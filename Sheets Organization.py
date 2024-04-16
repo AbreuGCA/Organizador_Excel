982c6f6ae771ccdfd771b62861216c567cff267d
AbreuGCA <gabrielabreuc.alencar@gmail.com>
2024-04-16 15:54:27 -0300

    Update Sheets Organization.py

--- Sheets Organization.py
+++ Sheets Organization.py
@@ -1,23 +1,32 @@
 import pandas as pd
 
-# Função para ler o arquivo CSV e escrever em outro arquivo CSV
-def processar_arquivo(input_file, output_file):
-    # Ler o arquivo CSV com ponto e vírgula como delimitador
-    df = pd.read_csv(input_file, delimiter=';')
+def processar_arquivos(input_files, output_file):
+    # Lista para armazenar os DataFrames de cada arquivo
+    dfs = []
+    
+    for input_file in input_files:
+        # Ler o arquivo CSV com ponto e vírgula como delimitador
+        df = pd.read_csv(input_file, delimiter=';', encoding='latin1')
+        dfs.append(df)
 
-    # Exibir as colunas presentes no DataFrame
-    print("Colunas presentes no arquivo CSV:")
-    print(df.columns)
+    # Concatenar os DataFrames
+    df_final = pd.concat(dfs, ignore_index=True)
+    
+    # Filtrar os dados para cidadãos de XPTO que frequentaram a escola e não tiveram dengue
+    df_filtrado = df_final[(df_final['Cidadão'] == 'XPTO') & (df_final['Frequentou Escola'] == 'Sim') & (df_final['Teve Dengue'] == 'Não')]
 
-    # Selecionar apenas as colunas desejadas: nome, data de nascimento e id
-    df_final = df[['Nome', 'Data de Nascimento', 'ID']]
+    # Escrever os dados processados em um único arquivo XLSX
+    df_filtrado.to_excel(output_file, index=False)
 
-    # Escrever os dados processados em outro arquivo XLSX
-    df_final.to_excel(output_file, index=False)
+# Lista de caminhos dos arquivos de entrada
+input_files = [
+    "C:\\Users\\gabri\\Downloads\\Base de dados AV1\\Terceira geração de bases\\Equipe4\\Base de Alunos3.csv",
+    "C:\\Users\\gabri\\Downloads\\Base de dados AV1\\Terceira geração de bases\\Equipe4\\Base de Dengue3.csv",
+    "C:\\Users\\gabri\\Downloads\\Base de dados AV1\\Terceira geração de bases\\Equipe4\\Base de Onibus3.csv"
+]
 
-# Caminho do arquivo de entrada e saída
-input_file = 'C:\\Users\\gabri\\Downloads\\Base de dados AV1\\Terceira geração de bases\\Equipe4\\Base de Alunos3.csv'
-output_file = 'C:\\Users\\gabri\\OneDrive\\Documentos\\Questão 1.xlsx'
+# Caminho do arquivo de saída
+output_file = "C:\\Users\\gabri\\OneDrive\\Documentos\\Questão 1.xlsx"
 
-# Chamada da função para processar o arquivo
-processar_arquivo(input_file, output_file)
+# Chamada da função para processar os arquivos
+processar_arquivos(input_files, output_file)
