98fa846491c75d0b7e801ffec3d73eb6cd4ffffb
AbreuGCA <gabrielabreuc.alencar@gmail.com>
2024-04-22 17:01:25 -0300

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -1,28 +1,39 @@
 import pandas as pd
 
-def processar_arquivo(input_file, output_file):
-    # Ler o arquivo Excel de entrada
-    df = pd.read_excel(input_file)
-
-    # Identificar as pessoas que tiveram dengue
-    pessoas_com_dengue = set(df[df['Data da Dengue'].notnull()]['Nome'])
-
-    # Filtrar as pessoas com base nas condições especificadas e remover duplicatas
-    df_filtrado = df[~((df['Data da Dengue'].notnull()) | ((df['Nome'].isin(pessoas_com_dengue)) &
-                       (df['Nome da Mae'].isin(df[df['Data da Dengue'].notnull()]['Nome da Mae'])) &
-                       (df['Nome do Pai'].isin(df[df['Data da Dengue'].notnull()]['Nome do Pai']))))]
-
-    # Remover duplicatas com base nos critérios especificados
-    df_filtrado = df_filtrado.drop_duplicates(subset=["Nome", "Nome da Mae", "Nome do Pai"])
-
+def processar_arquivo(input_files, output_file):
+    # Lista para armazenar os DataFrames de cada arquivo
+    dfs = []
+    for input_file in input_files:
+        # Ler o arquivo CSV com ponto e vírgula como delimitador e encoding latin1
+        df = pd.read_csv(input_file, delimiter=';', encoding='latin1')
+        dfs.append(df)
+    
+    # Concatenar os DataFrames
+    df_final = pd.concat(dfs, ignore_index=True)
+    
+    # Verificar se há duplicatas de nomes com informações de dengue
+    df_final['Duplicata_Dengue'] = df_final.duplicated(subset=['Nome'], keep=False) & (~df_final['Data da Dengue'].isnull())
+    
+    # Excluir pessoas com informações de dengue, sem informações na coluna "ID" ou duplicatas com dengue
+    df_final = df_final[~((~df_final['Data da Dengue'].isnull()) | df_final['ID'].isnull() | df_final['Duplicata_Dengue'])]
+    
+    # Selecionar colunas necessárias
+    df_final = df_final[['Nome', 'Data de Nascimento', 'ID']]
+    
+    # Remover duplicatas dos dados finais
+    df_final = df_final[~df_final.duplicated(subset=['Nome'], keep=False)]
+    
     # Escrever os dados processados no arquivo de saída
-    df_filtrado.to_excel(output_file, index=False)
+    df_final.to_excel(output_file, index=False)
 
-# Caminho do arquivo de entrada
-input_file = "C:\\Users\\gabri\\OneDrive\\Documentos\\Planilhas\\Conjunto Universo.xlsx"
+# Lista de caminhos dos arquivos de entrada
+input_files = [
+    "C:\\Users\\gabri\\OneDrive\\Documentos\\Planilhas\\Base de Dengue3.csv",
+    "C:\\Users\\gabri\\OneDrive\\Documentos\\Planilhas\\Base de Alunos3.csv"
+]
 
 # Caminho do arquivo de saída
 output_file = "C:\\Users\\gabri\\OneDrive\\Documentos\\Planilhas\\Questão 1.xlsx"
 
-# Chamada da função para processar o arquivo
-processar_arquivo(input_file, output_file)
+# Chamada da função para processar os arquivos
+processar_arquivo(input_files, output_file)
